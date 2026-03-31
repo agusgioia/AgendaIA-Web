1728b5e65b419fb52a43c54e0b3f0d17b82ca088
agusgioia <131310437+agusgioia@users.noreply.github.com>
2026-03-30 23:37:12 -0300

    Integrate Firebase messaging into service worker

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,29 @@
+/* global importScripts, firebase */
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js",
+);
+importScripts(
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js",
+);
+
+firebase.initializeApp({
+  apiKey: self.FIREBASE_API_KEY,
+  authDomain: self.FIREBASE_AUTH_DOMAIN,
+  projectId: self.FIREBASE_PROJECT_ID,
+  messagingSenderId: self.FIREBASE_MESSAGING_SENDER_ID,
+  appId: self.FIREBASE_APP_ID,
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(payload.notification.title, {
+    body: payload.notification.body,
+    icon: "/icon-192x192.png",
+  });
+});
+
+//cache
 const CACHE_NAME = "agenda-ia-v1";
 
 self.addEventListener("install", (e) => {
